1d20664bb29a5a77a648acb0485d0c56d15cbfab
Enrico Marino <enrico.marino@email.com>
2013-02-07 20:34:51 +0100

    Added test

---
+++ test/index.js
@@ -0,0 +1,18 @@
+var Router = require('router');
+var assert = require('component-assert');
+
+describe('Route#route', function () {
+
+  before(function () {
+  });
+
+  beforeEach(function () {
+  });
+
+  afterEach(function () {
+  });
+
+  it('should just work', function () {
+  });
+
+});
