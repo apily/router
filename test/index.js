2cc18c6ff86fb4923513100014640cfe15f129a9
Enrico Marino <enrico.marino@email.com>
2013-02-14 11:34:45 +0100

    update test

--- test/index.js
+++ test/index.js
@@ -8,57 +8,30 @@ describe('Route#route', function () {
     router = Router();
   });
 
-  beforeEach(function (done) {
-    console.log('start');
+  beforeEach(function () {
     window.location = '#';
-    setTimeout(function () {
-      router.history.start();
-      done();
-    }, 100);
+    router.history.start();
   });
 
-  afterEach(function (done) {
-    console.log('stop');
+  afterEach(function () {
     router.history.stop();
-    done();
   });
 
   it('should register a simple route', function (done) {
-    router.route('#a', 'a', function () { 
+    router.route('#b', function () { 
       done();
     });
-    setTimeout(function () {
-      window.location = '#z';
-      setTimeout(function () {
-        window.location = '#a';
-      }, 100);
-    }, 100);
+    window.location = '#a';
+    window.location = '#b';
   });
 
   it('should register a parametrized route', function (done) {
-    router.route('#hello/:name', 'hello', function (name) { 
+    router.route('#hello/:name', function (name) { 
       assert(name === 'enrico');
       done();
     });
-    setTimeout(function () {
-      window.location = '#z';
-      setTimeout(function () {
-        window.location = '#hello/enrico';
-      }, 100);
-    }, 100);
+    window.location = '#a';
+    window.location = '#hello/enrico';
   });
 
-  it('should emit route event', function (done) {
-    router.route('#hello/:name', 'hello', function () {});
-    router.on('route:hello', function (name) {
-      assert(name === 'enrico');
-      done();
-    });
-    setTimeout(function () {
-      window.location = '#z';
-      setTimeout(function () {
-        window.location = '#hello/enrico';
-      }, 100);
-    }, 100);
-  });
 });
